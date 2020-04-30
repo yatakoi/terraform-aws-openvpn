09eb72330770324e1e74ea9f8da4abd914ec332c
Maksim <allkall@yandex.ru>
2020-04-30 10:12:01 +1000

    Create aws-vpc.tf

---
+++ aws-vpc.tf
@@ -0,0 +1,12 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = var.access_key
+  secret_key  = var.secret_key
+  region      = var.region
+}
+
+/* Define our vpc */
+resource "aws_vpc" "default" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostn

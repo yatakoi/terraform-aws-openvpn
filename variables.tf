4e03be385e7694f3b403c7054b75ab2851e6b6a8
Maksim <allkall@yandex.ru>
2020-04-30 10:22:39 +1000

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,38 @@
+variable "access_key" { 
+  description = "AWS access key"
+  default     = "key"
+}
+
+variable "secret_key" { 
+  description = "AWS secret access key"
+  default     = "key"
+}
+
+variable "region"     { 
+  description = "AWS region to host your network"
+  default     = "us-west-1" 
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for VPC"
+  default     = "10.128.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for public subnet"
+  default     = "10.128.0.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR for private subnet"
+  default     = "10.128.1.0/24"
+}
+
+/* Ubuntu 18.04 LTS amis by region */
+variable "amis" {
+  description = "Base AMI to launch the instances with"
+default = {
+    us-west-1 = "ami-049d8641" 
+    us-east-1 = "ami-a6b8e7ce"
+  }
+}
